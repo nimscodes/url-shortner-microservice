7a5e41d9ca3e80cab03481df3085684c95007f9b
nimscodes <princenimako28@gmail.com>
2023-07-12 01:17:12 +0100

    project done

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const validator = require('validator');
+const cors = require("cors");
 const app = express();
 
 // URL Database and counter for short_url
 let urlDatabase = {};
 let id = 1;
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 // use the body-parser to parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("api/shorturl", (req, res) => {
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
 
+ // Validate the URL
+ if (!validator.isURL(originalUrl)) {
+  return res.status(400).json({ error: 'invalid url' });
+}
+
+  //check if url is already in the database
+  const foundKey = Object.keys(urlDatabase).find(
+    (key) => urlDatabase[key] === originalUrl);
+  if (foundKey) {
+    res.json({
+      original_url: originalUrl,
+      short_url: foundKey,
+    });
+  } else {
+    // assign the next id as short_urk and store it in the url database
+    urlDatabase[id] = originalUrl;
+    res.json({
+      original_url: originalUrl,
+      short_url: id,
+    });
+    id++;
+  }
 });
 
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shortUrl = req.params.shorturl;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.status(404).send("Short URL not found");
+  }
+});
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
